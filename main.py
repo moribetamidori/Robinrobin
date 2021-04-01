1dfae60cf5ce698c6f666e9e680e0bc038a064fa
Asimova <lucy1049684100@gmail.com>
2021-03-31 23:00:21 -0400

    ignore

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# something
+from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.cryptocurrencies import CryptoCurrencies
+import matplotlib.pyplot as plt
+from data import API_KEY
+
+ts = TimeSeries(key='API_KEY',output_format='pandas', indexing_type='date')
+# Get json object with the intraday data and another with  the call's metadata
+# data, meta_data = ts.get_intraday('GOOGL',interval='1min',outputsize='full')
+# print(data.head(2))
+cc = CryptoCurrencies(key='API_KEY', output_format='pandas')
+
